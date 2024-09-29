f60c7f93b85cc49d494000f999f6b02bb9a87dcf
Sergei Borja <76608956+wonderlust101@users.noreply.github.com>
2024-09-29 06:59:54 -0600

    Initial commit

---
+++ temp/vite.config.ts
@@ -0,0 +1,16 @@
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler'
+                additionalData: `@use "/src/assets/styles/abstract/_index.scss" as *;`
+            }
+        }
+    },
+    base: '//' //TODO: Change to git repository
+})
